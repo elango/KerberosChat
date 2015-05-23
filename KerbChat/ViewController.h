0e1a83d6c136f1cb72b76e85c7509f50bd5b8dc5
Anton Rodick <antonrodick@MacBook-Pro-Anton.local>
2015-05-23 13:24:15 +0300

    Initial Commit

---
+++ KerbChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KerbChat
+//
+//  Created by Anton Rodick on 23.05.15.
+//  Copyright (c) 2015 Anton Rodick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
